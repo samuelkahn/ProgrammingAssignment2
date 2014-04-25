9d32dca8c6adf19f66ccd95cde847724ca6d1583
Samuel Kahn <shkahn@ucsc.edu>
2014-04-25 05:55:00 -0700

    assignment answer

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+### i is the matrix inverse
+### when we call set(), we set x to new matrix and set the inverse to null again
+##getMatrix returns the Matrix, setInverse allows one to set inverse
+## getinverse gets the inverse of the matrix
+### note that we can define each internal function in one line
+### all the internal functions illustrate lexical scoping
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(m=matrix()){
+	i<-NULL
+	setMatrix<-function(x){
+		m<<-x
+		i<<-NULL
+	}
+	getMatrix<-function()m
+    setinverse <- function(inverse)i<<-inverse
+    getinverse<-function()i
+    list(setMatrix=setMatrix,get=getMatrix,
+             setInverse=setInverse,
+             setInverse=setInverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(m,...){
+        i<-m$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data<-x$getMatrix()
+        if(dim(data)[1]==dim(data)[2]){
+        	i<-solve(data,...)
+        	x$setinverse(i)
+        	return(m)
+        }else{
+        	message("Matrix not Square! Returning NULL")
+			return(NULL)
+		}
 }
